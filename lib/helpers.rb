6a70284800b793001df2214c3d299f805580e244
marc <marc.akbar@gmail.com>
2020-04-07 17:04:10 -0600

    Import materialize

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,21 +1,9 @@
 helpers do
 
-  def link_to url_fragment, mode=:path_only
-    case mode
-    when :path_only
-      base = request.script_name
-    when :full_url
-      if (request.scheme == 'http' && request.port == 80 ||
-          request.scheme == 'https' && request.port == 443)
-        port = ""
-      else
-        port = ":#{request.port}"
-      end
-      base = "#{request.scheme}://#{request.host}#{port}#{request.script_name}"
-    else
-      raise "Unknown script_url mode #{mode}"
-    end
-    "#{base}#{url_fragment}"
+  def link_to(text,url,opts={})
+    attributes = ""
+    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
   end
 
 end
