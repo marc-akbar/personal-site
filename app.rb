e40380f86065fa3e21c29733947abc61f9350535
marc <marc.akbar@gmail.com>
2020-04-07 18:59:11 -0600

    WIP layout

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sass/plugin/rack'
 require './lib/helpers'
+
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
 
 configure :development do
   register Sinatra::Reloader
