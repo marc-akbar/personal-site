f576bbc3f32c747e7088a02c84558aa481d1d9b9
marc <marc.akbar@gmail.com>
2020-04-14 17:16:09 -0600

    Prevent stars from overlapping text

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     delay: (el, i) => { return i * 400 }
   });
 
+  // Home page text animation
   anime({
     targets: '.home-text',
     opacity: '100%',
@@ -30,17 +31,20 @@ $(document).ready(function() {
   // Star population
   var width = $(".scene").width();
   var height = $(".scene").height();
+  var rightBound = width * 0.37  // right boundary for left star population
+  var leftBound = width * 0.63 // left boundary for right star population
   var stars = ['white-star-image', 'gold-star-image', 'teal-star-image'];
-  stars.forEach(populateStars);
 
-  function populateStars(value) {
-    for (var i = 0; i < 25; i++) {
-      // Pad the left and right by 30px
-      // 30 + (rand * (width of div - 60 - px of largest star))
-      var left = Math.floor(30 + (Math.random() * (width-70)));
-      // Pad the top by 90px and bottom by 42px
-      var top = Math.floor(90 + (Math.random() * (height-150)));
-      // Populate stars
+  stars.forEach(populateLeftStars);
+  stars.forEach(populateMiddleStars);
+  stars.forEach(populateRightStars);
+
+  function populateLeftStars(value) {
+    for (var i = 0; i < 10; i++) {
+      // Pad the left by 10px
+      var left = Math.floor(Math.random() * (rightBound - 10)) + 10;
+      // Pad the top by 90px and bottom by 150px
+      var top = Math.floor(Math.random() * ((height) - 240)) + 90;
       $(`<div class='${value}'></div>`).appendTo(".scene").css({
         left: left,
         top: top
@@ -48,32 +52,30 @@ $(document).ready(function() {
     }
   };
 
-  // document.querySelectorAll('div[class*="white-star-image"]').forEach(move);
-  //
-  // function move(value) {
-  //   anime({
-  //     targets: value,
-  //     easing: 'linear',
-  //     loop: true,
-  //     duration: 10000,
-  //     translateX: function(el) {
-  //       return width - ($(el).position().left);
-  //     },
-  //     translateY: function(el) {
-  //       return height - ($(el).position().top);
-  //     }
-  //   });
-  // }
-
-  anime({
-    targets: '.white-star-image',
-    easing: 'linear',
-    loop: true,
-    rotate: {
-      value: 360,
-      duration: 5000,
+  function populateMiddleStars(value) {
+    for (var i = 0; i < 10; i++) {
+      // Pad the left and right by 10px
+      var left = Math.floor(Math.random() * (width - 25)) + 10;
+      // Pad the top by 300px and bottom by 50px
+      var top = Math.floor(Math.random() * (height - 350)) + 300;
+      $(`<div class='${value}'></div>`).appendTo(".scene").css({
+        left: left,
+        top: top
+      });
     }
-  });
+  };
+
+  function populateRightStars(value) {
+    for (var i = 0; i < 10; i++) {
+      // Pad the right by 10px
+      var left = Math.floor(Math.random() * (width - leftBound - 25)) + leftBound - 25;
+      var top = Math.floor(Math.random() * (height - 240)) + 90;
+      $(`<div class='${value}'></div>`).appendTo(".scene").css({
+        left: left,
+        top: top
+      });
+    }
+  };
 
   anime({
     targets: '.gold-star-image',
@@ -81,7 +83,7 @@ $(document).ready(function() {
     loop: true,
     rotate: {
       value: 360,
-      duration: 6000,
+      duration: 4000,
     }
   });
 
