03909bd632a86244b031554ebb9f1b7737e1d560
marc <marc.akbar@gmail.com>
2020-04-14 12:41:43 -0600

    Add scramble text and WIP title drawing

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,95 @@
 $(document).ready(function() {
+  // Materialize initializations
   $('.sidenav').sidenav();
   $('.parallax').parallax();
 
-  const svgPath = document.querySelectorAll('.path');
-  const svgText = anime({
+  // Title drawing animation
+  const svgPath = document.querySelectorAll('path');
+  anime({
     targets: svgPath,
     loop: true,
     direction: 'alternate',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 700,
+    duration: 1000,
     delay: (el, i) => { return i * 500 }
   });
 
+  // Scramble text
+  class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = '!<>-_\\/[]{}—=+*^?#________'
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 100)
+        const end = start + Math.floor(Math.random() * 100)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+
+  const phrases = [
+    'developer',
+    'climber',
+    'learner'
+  ]
+
+  const el = document.querySelector('.scramble')
+  const fx = new TextScramble(el)
+
+  let counter = 0
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 4000)
+    })
+    counter = (counter + 1) % phrases.length
+  }
+
+  next()
+
+  // Star population
   var width = $(".scene").width();
   var height = $(".scene").height();
   var stars = ['white-star-image', 'gold-star-image', 'teal-star-image'];
