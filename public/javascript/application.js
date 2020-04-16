212e3aacf5dc2ad6f3fff50e7e24668160d16b0d
marc <marc.akbar@gmail.com>
2020-04-15 23:45:45 -0600

    Add lightbox and compress image

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Materialize initializations
   $('.sidenav').sidenav();
-  $('.parallax').parallax();
+  $('.materialboxed').materialbox();
 
   // Title drawing animation
   var svgPath = document.querySelectorAll('.path');
