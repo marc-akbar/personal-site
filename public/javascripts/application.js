550590c7b336f944dbf5d5eeaef76d586bec911c
marc <marc.akbar@gmail.com>
2020-04-27 17:42:45 -0600

    Change scene in click, reorganize js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,94 +1,95 @@
 $(document).ready(function() {
-  // Remove blank page when page loads
+  // Remove loading page when page loads
   $("#loading").remove();
-
   // Materialize initializations
   $('.sidenav').sidenav();
   $('.materialboxed').materialbox();
 
-  // Toggle day/night
-  $('.time-toggle').on('click',function(){
-		$(this).text() === 'brightness_2' ? $(this).text('wb_sunny') : $(this).text('brightness_2');
+  function runClouds() {
+    // Populate and ease in clouds
+    $(`<div class='left-cloud'></div>`).appendTo("#scene.day");
+    $(`<div class='right-cloud'></div>`).appendTo("#scene.day");
+    $(`<div class='bottom-cloud'></div>`).appendTo("#scene.day");
 
-    localStorage.setItem('mode', (localStorage.getItem('mode') || 'night') === 'night' ? 'day' : 'night');
-    if (localStorage.getItem('mode') === 'day') {
-      var el = document.querySelectorAll('.day')
-      el.forEach(function(value) {value.classList.add('night')})
-      el.forEach(function(value) {value.classList.remove('day')})
-    } else {
-      var el = document.querySelectorAll('.night')
-      el.forEach(function(value) {value.classList.add('day')})
-      el.forEach(function(value) {value.classList.remove('night')})
+    var cloudImages = document.querySelectorAll('div[class*="-cloud"]')
+    anime({
+      targets: cloudImages,
+      translateY: ['100%', '0%'],
+      easing: 'easeOutSine',
+      duration: 1500
+    });
+    anime({
+      targets: cloudImages,
+      opacity: '100%',
+      easing: 'easeInQuad',
+      duration: 1000
+    });
+  };
+
+  function runStars() {
+    // Star population
+    var width = $("#scene").width();
+    var height = $("#scene").height();
+    var rightBound = width * 0.37  // right boundary for left star population
+    var leftBound = width * 0.63 // left boundary for right star population
+    var stars = ['white-star-image', 'gold-star-image', 'teal-star-image'];
+
+    stars.forEach(populateLeftStars);
+    stars.forEach(populateMiddleStars);
+    stars.forEach(populateRightStars);
+
+    function populateLeftStars(value) {
+      for (var i = 0; i < (width/100); i++) {
+        // Pad the left by 10px
+        var left = Math.floor(Math.random() * (rightBound - 10)) + 10;
+        var top = Math.floor(Math.random() * ((height) - (height * 0.3))) + (height * 0.15);
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
+          left: left,
+          top: top
+        });
+      }
+    };
+    function populateMiddleStars(value) {
+      for (var i = 0; i < (width/100); i++) {
+        // Pad the left and right by 10px
+        var left = Math.floor(Math.random() * (width - 25)) + 10;
+        var top = Math.floor(Math.random() * (height - (height * 0.59))) + (height * 0.5);
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
+          left: left,
+          top: top
+        });
+      }
+    };
+    function populateRightStars(value) {
+      for (var i = 0; i < (width/100); i++) {
+        // Pad the right by 10px
+        var left = Math.floor(Math.random() * (width - leftBound - 25)) + leftBound - 25;
+        var top = Math.floor(Math.random() * (height - (height * 0.3))) + (height * 0.15);
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
+          left: left,
+          top: top
+        });
+      }
     };
 
-    console.log(localStorage.getItem('mode'));
-	});
-
-  // Star population
-  var width = $("#scene").width();
-  var height = $("#scene").height();
-  var rightBound = width * 0.37  // right boundary for left star population
-  var leftBound = width * 0.63 // left boundary for right star population
-  var stars = ['white-star-image', 'gold-star-image', 'teal-star-image'];
-
-  stars.forEach(populateLeftStars);
-  stars.forEach(populateMiddleStars);
-  stars.forEach(populateRightStars);
-
-  function populateLeftStars(value) {
-    for (var i = 0; i < (width/100); i++) {
-      // Pad the left by 10px
-      var left = Math.floor(Math.random() * (rightBound - 10)) + 10;
-      var top = Math.floor(Math.random() * ((height) - (height * 0.3))) + (height * 0.15);
-      $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-        left: left,
-        top: top
-      });
-    }
+    anime({
+      targets: '.gold-star-image',
+      loop: true,
+      rotate: 360,
+      duration: 4000,
+      easing: 'linear'
+    });
+    anime({
+      targets: '.teal-star-image',
+      loop: true,
+      rotate: 360,
+      duration: 9000,
+      easing: 'linear'
+    });
   };
 
-  function populateMiddleStars(value) {
-    for (var i = 0; i < (width/100); i++) {
-      // Pad the left and right by 10px
-      var left = Math.floor(Math.random() * (width - 25)) + 10;
-      var top = Math.floor(Math.random() * (height - (height * 0.59))) + (height * 0.5);
-      $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-        left: left,
-        top: top
-      });
-    }
-  };
-
-  function populateRightStars(value) {
-    for (var i = 0; i < (width/100); i++) {
-      // Pad the right by 10px
-      var left = Math.floor(Math.random() * (width - leftBound - 25)) + leftBound - 25;
-      var top = Math.floor(Math.random() * (height - (height * 0.3))) + (height * 0.15);
-      $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-        left: left,
-        top: top
-      });
-    }
-  };
-
-  // Populate and ease in clouds
-  $(`<div class='left-cloud'></div>`).appendTo("#scene.day");
-  $(`<div class='right-cloud'></div>`).appendTo("#scene.day");
-  $(`<div class='bottom-cloud'></div>`).appendTo("#scene.day");
-
-  var cloudImages = document.querySelectorAll('div[class*="-cloud"]')
-  anime({
-    targets: cloudImages,
-    translateY: ['100%', '0%'],
-    easing: 'easeOutSine',
-    duration: 1500
-  });
-  anime({
-    targets: cloudImages,
-    opacity: '100%',
-    easing: 'easeInQuad',
-    duration: 1000
-  });
+  runStars();
+  localStorage.setItem('mode', 'night')
 
   if (window.location.pathname === "/") {
     // Title drawing animation
@@ -106,20 +107,19 @@ $(document).ready(function() {
     var greetingText = greetingScene.querySelector('.greeting-text');
     var greetingMountain = greetingScene.querySelector('.mountain-image');
     var greetingStars = greetingScene.querySelectorAll('div[class*="-star-"]');
+
     anime({
       targets: greetingText,
       opacity: '100%',
       easing: 'easeInSine',
       delay: 2000
     });
-
     anime({
       targets: [greetingMountain, 'nav'],
       opacity: '100%',
       easing: 'easeInSine',
       delay: 3000
     });
-
     anime({
       targets: greetingStars,
       opacity: '100%',
@@ -128,7 +128,6 @@ $(document).ready(function() {
     });
 
   } else {
-
     // Star rotation and easing outside greeting scene
     var starImages = document.querySelectorAll('div[class*="-star-"]')
     anime({
@@ -139,19 +138,28 @@ $(document).ready(function() {
     });
   };
 
-  anime({
-    targets: '.gold-star-image',
-    loop: true,
-    rotate: 360,
-    duration: 4000,
-    easing: 'linear'
-  });
+  // Toggle day/night
+  $('.time-toggle').on('click',function(){
+		if ($(this).text() === 'brightness_2') {
+      $(this).text('wb_sunny');
+      localStorage.setItem('mode', 'day')
+    } else {
+      $(this).text('brightness_2');
+      localStorage.setItem('mode', 'night')
+    };
 
-  anime({
-    targets: '.teal-star-image',
-    loop: true,
-    rotate: 360,
-    duration: 9000,
-    easing: 'linear'
-  });
+    if (localStorage.getItem('mode') === 'day') {
+      var el = document.querySelectorAll('.day')
+      el.forEach(function(value) {value.classList.add('night')})
+      el.forEach(function(value) {value.classList.remove('day')})
+      $('div[class*="-cloud"]').hide();
+      runStars();
+    } else {
+      var el = document.querySelectorAll('.night')
+      el.forEach(function(value) {value.classList.add('day')})
+      el.forEach(function(value) {value.classList.remove('night')})
+      $('div[class*="-star-"]').hide();
+      runClouds();
+    };
+	});
 });
