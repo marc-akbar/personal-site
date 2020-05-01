caf1919c270e62fbf022340b0309d60b8bba8a1e
marc <marc.akbar@gmail.com>
2020-04-30 21:05:56 -0600

    Clean up and change styling
    - Format css and minify js
    - Change title and logo colors on scene
    - Change button color on night

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,49 +2,31 @@ $(document).ready(function() {
   // Remove loading after scene swap
   setTimeout(function() {
     $("#loading").remove();
-  }, 500);
+  }, 300);
   // Materialize initializations
   $('.sidenav').sidenav();
-  // Set initial time to day
-  localStorage.setItem('mode', localStorage.getItem('mode') || 'day')
+  // Set initial time
+  localStorage.setItem('mode', localStorage.getItem('mode') || 'day');
   // Load appropriate scene
   pathcheck: if (localStorage.getItem('mode') === 'night') {
     $('#dn').prop('checked', true);
-    swapToNight();
-    populateStars();
-    // Run greeting if index path
-    if (window.location.pathname === "/") {
-      greetingAnimation();
-      break pathcheck;
-    };
-    easeInStars();
-    rotateStars();
+    swapToNight(); populateStars();
+    if (window.location.pathname === "/") { greetingAnimation(); break pathcheck; };
+    easeInStars(); rotateStars();
   } else {
     $('#dn').prop('checked', false);
-    swapToDay();
-    populateClouds();
-    // Run greeting if index path
-    if (window.location.pathname === "/") {
-      greetingAnimation();
-      break pathcheck;
-    };
-    easeInClouds();
-    raiseClouds();
+    swapToDay(); populateClouds();
+    if (window.location.pathname === "/") { greetingAnimation(); break pathcheck; };
+    easeInClouds(); raiseClouds();
   };
 
   // Toggle day/night on switch
   $('#dn').on('change',function(){
 		$(this).is(':checked') ? localStorage.setItem('mode', 'night') : localStorage.setItem('mode', 'day');
     if (localStorage.getItem('mode') === 'night') {
-       swapToNight();
-       populateStars();
-       easeInStars();
-       rotateStars();
+       swapToNight(); populateStars(); easeInStars(); rotateStars();
      } else {
-       swapToDay();
-       populateClouds();
-       easeInClouds();
-       raiseClouds();
+       swapToDay(); populateClouds(); easeInClouds(); raiseClouds();
      };
 	});
 
@@ -52,7 +34,6 @@ $(document).ready(function() {
     var tl = gsap.timeline();
     var lCloud = $('.left-cloud');
     var sunClouds = $('.sun-cloud');
-
     tl.from('#greeting-scene .greeting-text', { opacity: 0, ease: 'power2.in', duration: 1, delay: 2 })
       .from(['#greeting-scene .mountain-image', '.nav-wrapper'], { opacity: 0, ease: 'none', duration: 1 })
       .addLabel('runScene', "-=1");
@@ -62,9 +43,12 @@ $(document).ready(function() {
         .from('.gold-star-image', { repeat: -1, rotation: -360, duration: 5, ease: 'none' }, 'runScene')
         .from('.teal-star-image', { repeat: -1, rotation: -360, duration: 10, ease: 'none' }, 'runScene');
     } else {
-      tl.from(['.left-cloud', '.right-cloud', '.sun-cloud'], { opacity: 0, ease: 'power3.in', duration: 1 }, 'runScene')
-        .from(['.left-cloud', '.right-cloud'], { y: (lCloud.height() * 1.5), ease: 'power2.out', duration: 2 }, 'runScene')
-        .from(sunClouds, { y: (sunClouds.height() * 1.5), ease: 'power2.out', duration: 2 }, 'runScene');
+      tl.from(['#greeting-scene .left-cloud', '#greeting-scene .right-cloud', '#greeting-scene .sun-cloud'], { opacity: 0, ease: 'power3.in', duration: 1 }, 'runScene')
+        .from(['#greeting-scene .left-cloud', '#greeting-scene .right-cloud'], { y: (lCloud.height() * 1.5), ease: 'power2.out', duration: 2 }, 'runScene')
+        .from('#greeting-scene .sun-cloud', { y: (sunClouds.height() * 1.5), ease: 'power2.out', duration: 2 }, 'runScene');
+      gsap.from(['#say-hi .left-cloud', '#say-hi .right-cloud', '#say-hi .sun-cloud'], { opacity: 0, ease: 'power3.in', duration: 1 });
+      gsap.from(['#say-hi .left-cloud', '#say-hi .right-cloud'], { y: (lCloud.height() * 1.5), ease: 'power2.out', duration: 2 });
+      gsap.from('#say-hi .sun-cloud', { y: (sunClouds.height() * 1.5), ease: 'power2.out', duration: 2 });
     };
   };
 
@@ -92,31 +76,13 @@ $(document).ready(function() {
     $("<div class='sun-cloud'></div>").appendTo("#say-hi");
   };
 
-  function easeInClouds() {
-    gsap.from(['.left-cloud', '.right-cloud', '.sun-cloud'], {
-      opacity: 0,
-      ease: 'power3.in',
-      duration: 1,
-      delay: .3
-    });
-  };
+  function easeInClouds() { gsap.from(['.left-cloud', '.right-cloud', '.sun-cloud'], { opacity: 0, ease: 'power3.in', duration: 1, delay: .2 }) };
 
   function raiseClouds() {
     var lCloud = $('.left-cloud');
     var sunClouds = $('.sun-cloud');
-
-    gsap.from(['.left-cloud', '.right-cloud'], {
-      y: (lCloud.height() * 1.5),
-      ease: 'power2.out',
-      duration: 2,
-      delay: .3
-    });
-    gsap.from(sunClouds, {
-      y: (sunClouds.height() * 1.5),
-      ease: 'power2.out',
-      duration: 2,
-      delay: .3
-    });
+    gsap.from(['.left-cloud', '.right-cloud'], { y: (lCloud.height() * 1.5), ease: 'power2.out', duration: 2, delay: .2 });
+    gsap.from(sunClouds, { y: (sunClouds.height() * 1.5), ease: 'power2.out', duration: 2, delay: .2 });
   };
 
   function populateStars() {
@@ -138,10 +104,7 @@ $(document).ready(function() {
         // Pad the left by 10px
         var left = Math.floor(Math.random() * (rightBound - 10)) + 10;
         var top = Math.floor(Math.random() * ((sceneHeight) - (sceneHeight * 0.3))) + (sceneHeight * 0.15);
-        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-          left: left,
-          top: top
-        });
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({ left: left, top: top });
       }
     };
     function populateMiddleStars(value) {
@@ -149,10 +112,7 @@ $(document).ready(function() {
         // Pad the left and right by 10px
         var left = Math.floor(Math.random() * (width - 25)) + 10;
         var top = Math.floor(Math.random() * (sceneHeight - (sceneHeight * 0.59))) + (sceneHeight * 0.5);
-        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-          left: left,
-          top: top
-        });
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({ left: left, top: top });
       }
     };
     function populateRightStars(value) {
@@ -160,10 +120,7 @@ $(document).ready(function() {
         // Pad the right by 10px
         var left = Math.floor(Math.random() * (width - leftBound - 25)) + leftBound - 25;
         var top = Math.floor(Math.random() * (sceneHeight - (sceneHeight * 0.3))) + (sceneHeight * 0.15);
-        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({
-          left: left,
-          top: top
-        });
+        $(`<div class='${value}'></div>`).appendTo("#scene.night").css({ left: left, top: top });
       }
     };
     function populateFooterStars(value) {
@@ -171,37 +128,15 @@ $(document).ready(function() {
         // Pad the left and right by 10px
         var left = Math.floor(Math.random() * (width - 25)) + 10;
         var top = Math.floor(Math.random() * (footerHeight - 25)) + 25;
-        $(`<div class='${value}'></div>`).appendTo("#say-hi").css({
-          left: left,
-          top: top
-        });
+        $(`<div class='${value}'></div>`).appendTo("#say-hi").css({ left: left, top: top });
       }
     };
   };
 
-  function easeInStars() {
-    gsap.from(['.white-star-image', '.gold-star-image', '.teal-star-image'], {
-      opacity: 0,
-      ease: 'power3.in',
-      duration: 1,
-      delay: .3
-    })
-  };
+  function easeInStars() { gsap.from(['.white-star-image', '.gold-star-image', '.teal-star-image'], { opacity: 0, ease: 'power3.in', duration: 1, delay: .2 }) };
 
   function rotateStars() {
-    gsap.from('.gold-star-image', {
-      repeat: -1,
-      rotation: -360,
-      duration: 5,
-      ease: 'none',
-      delay: .3
-    });
-    gsap.from('.teal-star-image', {
-      repeat: -1,
-      rotation: -360,
-      duration: 10,
-      ease: 'none',
-      delay: .3
-    });
+    gsap.from('.gold-star-image', { repeat: -1, rotation: -360, duration: 5, ease: 'none', delay: .2 });
+    gsap.from('.teal-star-image', { repeat: -1, rotation: -360, duration: 10, ease: 'none', delay: .2 });
   };
 });
