c21ca126b591315819b52785125c43823e58db52
marc <marc.akbar@gmail.com>
2020-05-04 16:03:06 -0600

    Swap class before page load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  // Remove loading after scene swap
-  $("#loading").remove();
   // Materialize initializations
   $('.sidenav').sidenav();
   // Load appropriate scene
